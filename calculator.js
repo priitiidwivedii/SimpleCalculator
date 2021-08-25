cec2d8f582435593fcf4c43d6d6fd0c599cd8dc3
pritiidwivedii <pritiidwivedii@gmail.com>
2021-08-26 02:41:35 +0530

    first commit

---
+++ calculator.js
@@ -0,0 +1,43 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res){
+
+var num1 = Number(req.body.num1);
+var num2 = Number(req.body.num2);
+
+var result = num1 + num2;
+
+res.send("The result of a calculation is " + result);
+});
+
+app.get("/BMICalculator", function(req, res){
+  res.sendFile(__dirname + "/BMICalculator.html");
+});
+
+app.post("/BMICalculator", function(req, res){
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
+
+  var bmi = weight/ (height * height);
+
+  res.send("Your BMI is " + bmi);
+
+});
+
+
+
+app.listen(3000, function(){
+
+  console.log("Server is running on port 3000");
+
+});
